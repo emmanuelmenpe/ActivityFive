1784dd0e6870d47a1ff72ab14d352c99f60f7878
emmanuel menpe <menpemmanuel30@gmail.com>
2022-06-04 09:59:22 -0500

    conexión a BD

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-/*const express = require('express');
 const conectarDB = require('./config/db');
+/*const express = require('express');
+
 const usuarioRoute = require('./routes/usuarios');
 const authRoute = require('./routes/auth');
 const proyectoRoute = require('./routes/proyectos');
@@ -7,9 +8,9 @@ const tareaRoute = require('./routes/tareas');
 const cors = require('cors');
 
 const app = express();
-
+*/
 conectarDB();
-
+/*
 //habilitar express.json
 app.use(express.json({extended: true}));//lo explica en el video 335 del curso
 
