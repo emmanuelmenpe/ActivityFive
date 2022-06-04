6094fe2c8aa69e426491b6b185e3830c30c9efc0
emmanuel menpe <menpemmanuel30@gmail.com>
2022-06-04 09:28:51 -0500

    creación de carpetas

---
+++ index.js
@@ -0,0 +1,30 @@
+/*const express = require('express');
+const conectarDB = require('./config/db');
+const usuarioRoute = require('./routes/usuarios');
+const authRoute = require('./routes/auth');
+const proyectoRoute = require('./routes/proyectos');
+const tareaRoute = require('./routes/tareas');
+const cors = require('cors');
+
+const app = express();
+
+conectarDB();
+
+//habilitar express.json
+app.use(express.json({extended: true}));//lo explica en el video 335 del curso
+
+const PORT = process.env.PORT || 4000;
+
+//habilitar cors
+app.use(cors());
+
+//definir rutas
+app.use('/api/usuarios', usuarioRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/proyectos', proyectoRoute);
+app.use('/api/tareas', tareaRoute);
+
+//iniciar app
+app.listen(PORT, () => {
+    console.log(`servidor ejecutandose en puerto ${PORT}`);
+});*/
